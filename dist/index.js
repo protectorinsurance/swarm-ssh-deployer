fb206cda8b88079aefef585b73d733c9b11a0839
Stian Hegglund <stian.hegglund@protectorforsikring.no>
2021-09-09 11:27:12 +0200

    Testing deploy

--- dist/index.js
+++ dist/index.js
@@ -20,7 +20,7 @@ async function run() {
     const port = core.getInput("port");
     const ghcrUsername = core.getInput("ghcr-username");
     const ghcrToken = core.getInput("ghcr-token");
-    const deployCommand = `docker login -u ${ghcrUsername} -p ${ghcrToken} ghcr.io && docker service update --with-registry-auth --image ${image}:${imageTag} ${serviceName}"`;
+    const deployCommand = `"docker login -u ${ghcrUsername} -p ${ghcrToken} ghcr.io && docker service update --with-registry-auth --image ${image}:${imageTag} ${serviceName}"`;
 
     core.info("Starting to deploy");
 
@@ -28,7 +28,7 @@ async function run() {
     conn
       .on("ready", () => {
         console.log("Client :: ready");
-        conn.exec('uptime', (err, stream) => {
+        conn.exec(deployCommand, (err, stream) => {
           if (err) core.setFailed(err);
           stream
             .on("close", (code, signal) => {
