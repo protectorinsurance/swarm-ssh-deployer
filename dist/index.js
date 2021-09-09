c6651a5279bb8ba0d766329e29de1b102053b3b2
Stian Hegglund <stian.hegglund@protectorforsikring.no>
2021-09-09 12:11:09 +0200

    Try simpler command

--- dist/index.js
+++ dist/index.js
@@ -29,7 +29,7 @@ async function run() {
     conn
       .on("ready", () => {
         console.log("Client :: ready");
-        conn.exec(deployCommand, (err, stream) => {
+        conn.exec(`la -la`, (err, stream) => {
           if (err) core.setFailed(err);
           stream
             .on("close", (code, signal) => {
