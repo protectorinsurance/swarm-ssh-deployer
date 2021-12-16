52d0f7ed2e2ffc3929c1c62844bd23b4438ca2ec
anin99 <andre.ingvaldsen@protectorforsikring.no>
2021-12-16 16:21:14 +0100

    fix for dist

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,24 @@ require('./sourcemap-register.js');module.exports =
 const core = __nccwpck_require__(2186);
 const { readFileSync } = __nccwpck_require__(5747);
 const { Client } = __nccwpck_require__(5869);
-const parseInputs = __nccwpck_require__(2303)
+
+function parseInputs() {
+    const serviceName = core.getInput("service-name");
+    const image = core.getInput("image");
+    const imageTag = core.getInput("image-tag");
+    const privateKey = core.getInput("private-key");
+    const username = core.getInput("username");
+    const hostname = core.getInput("hostname");
+    const port = core.getInput("port");
+    const ghcrUsername = core.getInput("ghcr-username");
+    const ghcrToken = core.getInput("ghcr-token");
+    const loginCommand = `docker login -u ${ghcrUsername} -p ${ghcrToken} ghcr.io`;
+    const args = core.getInput("args")
+    const serviceCommand = `docker service update --with-registry-auth ${args} --image ${image}:${imageTag} ${serviceName}`;
+    const deployCommand = `${loginCommand} && ${serviceCommand}`;
+
+    return {privateKey, username, hostname, port, deployCommand};
+}
 
 async function run() {
   try {
@@ -22749,14 +22766,6 @@ module.exports = require(__nccwpck_require__.ab + "build/Release/cpufeatures.nod
 /***/ ((module) => {
 
 module.exports = eval("require")("./crypto/build/Release/sshcrypto.node");
-
-
-/***/ }),
-
-/***/ 2303:
-/***/ ((module) => {
-
-module.exports = eval("require")("parse-inputs");
 
 
 /***/ }),
