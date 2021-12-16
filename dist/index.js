38524d02b2c6a7cc9692158b337d5fd0a8413346
anin99 <andre.ingvaldsen@protectorforsikring.no>
2021-12-16 16:43:40 +0100

    fix import to fix build

--- dist/index.js
+++ dist/index.js
@@ -8,24 +8,7 @@ require('./sourcemap-register.js');module.exports =
 const core = __nccwpck_require__(2186);
 const { readFileSync } = __nccwpck_require__(5747);
 const { Client } = __nccwpck_require__(5869);
-
-function parseInputs() {
-    const serviceName = core.getInput("service-name");
-    const image = core.getInput("image");
-    const imageTag = core.getInput("image-tag");
-    const privateKey = core.getInput("private-key");
-    const username = core.getInput("username");
-    const hostname = core.getInput("hostname");
-    const port = core.getInput("port");
-    const ghcrUsername = core.getInput("ghcr-username");
-    const ghcrToken = core.getInput("ghcr-token");
-    const loginCommand = `docker login -u ${ghcrUsername} -p ${ghcrToken} ghcr.io`;
-    const args = core.getInput("args")
-    const serviceCommand = `docker service update --with-registry-auth ${args} --image ${image}:${imageTag} ${serviceName}`;
-    const deployCommand = `${loginCommand} && ${serviceCommand}`;
-
-    return {privateKey, username, hostname, port, deployCommand};
-}
+const parseInputs = __nccwpck_require__(6591)
 
 async function run() {
   try {
@@ -22751,6 +22734,34 @@ nacl.setPRNG = function(fn) {
 })();
 
 })( true && module.exports ? module.exports : (self.nacl = self.nacl || {}));
+
+
+/***/ }),
+
+/***/ 6591:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const core = __nccwpck_require__(2186);
+
+function parseInputs() {
+    const serviceName = core.getInput("service-name");
+    const image = core.getInput("image");
+    const imageTag = core.getInput("image-tag");
+    const privateKey = core.getInput("private-key");
+    const username = core.getInput("username");
+    const hostname = core.getInput("hostname");
+    const port = core.getInput("port");
+    const ghcrUsername = core.getInput("ghcr-username");
+    const ghcrToken = core.getInput("ghcr-token");
+    const loginCommand = `docker login -u ${ghcrUsername} -p ${ghcrToken} ghcr.io`;
+    const args = core.getInput("args")
+    const serviceCommand = `docker service update --with-registry-auth ${args} --image ${image}:${imageTag} ${serviceName}`;
+    const deployCommand = `${loginCommand} && ${serviceCommand}`;
+
+    return {privateKey, username, hostname, port, deployCommand};
+}
+
+module.exports = parseInputs;
 
 
 /***/ }),
--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 const core = require("@actions/core");
 const { readFileSync } = require("fs");
 const { Client } = require("ssh2");
-
-function parseInputs() {
-    const serviceName = core.getInput("service-name");
-    const image = core.getInput("image");
-    const imageTag = core.getInput("image-tag");
-    const privateKey = core.getInput("private-key");
-    const username = core.getInput("username");
-    const hostname = core.getInput("hostname");
-    const port = core.getInput("port");
-    const ghcrUsername = core.getInput("ghcr-username");
-    const ghcrToken = core.getInput("ghcr-token");
-    const loginCommand = `docker login -u ${ghcrUsername} -p ${ghcrToken} ghcr.io`;
-    const args = core.getInput("args")
-    const serviceCommand = `docker service update --with-registry-auth ${args} --image ${image}:${imageTag} ${serviceName}`;
-    const deployCommand = `${loginCommand} && ${serviceCommand}`;
-
-    return {privateKey, username, hostname, port, deployCommand};
-}
+const parseInputs = require('./parse-inputs')
 
 async function run() {
   try {
